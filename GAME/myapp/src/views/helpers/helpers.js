04946e42786443b2a7b97936f2a360ded4713945
luanacarlos <luan.alencar@icomp.ufam.edu.br>
2023-10-28 17:16:27 -0400

    atualizando

---
+++ GAME/myapp/src/views/helpers/helpers.js
@@ -0,0 +1,8 @@
+const toLower = function(value) {
+    return value.toLowerCase();
+}
+const toUpper = function(value) {
+    return value.toUpperCase();
+}
+
+module.exports = { toLower, toUpper };
